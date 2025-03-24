8833bed323ae55826ece3201966379a023869e7e
PranavRM5 <pranavmahajan510@gmail.com>
2025-03-24 17:51:20 +0530

    Web DevTool

---
+++ JavaScript/JS_Chapter_1/JS_2/variables.js
@@ -0,0 +1,19 @@
+console.log("hello pranav");
+
+var a = 10;
+console.log(a);
+
+a = "Pranav";
+console.log(a);
+
+let b = 20;
+console.log(b);
+
+b = "Kunal";
+console.log(b);
+
+const c = 30;
+console.log(c);
+
+// c = "Yash";
+// console.log(c);
